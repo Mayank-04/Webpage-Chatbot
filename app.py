688ddbbbb8aa9674aaeb3c965b6edeed929b4fc5
Mayank Gautam <123808179+Mayank-04@users.noreply.github.com>
2025-06-20 19:28:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# groq_api_key = os.getenv('GROQ_API_KEY')
-groq_api_key = st.secrets["GROQ_API_KEY"]
+groq_api_key = os.getenv('GROQ_API_KEY')
+# groq_api_key = st.secrets["GROQ_API_KEY"]
 
 st.title("Website ChatBot with Llama3 and Groq")
 
